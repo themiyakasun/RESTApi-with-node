291c857d95efa265b5a500cbd0a8da36001f089d
themiyakasun <themiyakasun.tb@gmail.com>
2024-06-17 19:58:44 +0530

    Testing client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('/users')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        users.map((user, index) => <p key={index}>{user.firstName}</p>)
+      )}
+    </div>
+  );
 };
 
 export default App;
